c99634eccdc54b8bbe2324a82cc92fe944e18763
Greg <greg@w3visi.com>
2014-11-09 18:43:13 -0500

    Added image custom post type

--- lib/post-types.php
+++ lib/post-types.php
@@ -1,11 +1,11 @@
 <?php
 
-/*********************
-MANIFESTO CPT
-*********************/
 // Register Custom Post Type
 function val_post_type() {
 
+	/*********************
+	MANIFESTO CPT
+	*********************/
 	$labels = array(
 		'name'                => _x( 'Manifesto Items', 'Post Type General Name', 'text_domain' ),
 		'singular_name'       => _x( 'Manifesto Item', 'Post Type Singular Name', 'text_domain' ),
@@ -50,6 +50,9 @@ function val_post_type() {
 	register_post_type( 'manifesto', $args );
 
 
+	/*********************
+	PRESS RELEASE CPT
+	*********************/
 	$labels = array(
 		'name'                => _x( 'Press Releases', 'Post Type General Name', 'text_domain' ),
 		'singular_name'       => _x( 'Press Release', 'Post Type Singular Name', 'text_domain' ),
@@ -93,6 +96,51 @@ function val_post_type() {
 	);
 	register_post_type( 'press_release', $args );
 
+	/*********************
+	IMAGE CPT
+	*********************/
+	$labels = array(
+		'name'                => _x( 'Images', 'Post Type General Name', 'text_domain' ),
+		'singular_name'       => _x( 'Image', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'           => __( 'Images', 'text_domain' ),
+		'parent_item_colon'   => __( 'Parent Image:', 'text_domain' ),
+		'all_items'           => __( 'Images', 'text_domain' ),
+		'view_item'           => __( 'View Image', 'text_domain' ),
+		'add_new_item'        => __( 'Add New Image', 'text_domain' ),
+		'add_new'             => __( 'Add New', 'text_domain' ),
+		'edit_item'           => __( 'Edit Image', 'text_domain' ),
+		'update_item'         => __( 'Update Image', 'text_domain' ),
+		'search_items'        => __( 'Search Images', 'text_domain' ),
+		'not_found'           => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
+	);
+	$rewrite = array(
+		'slug'                => 'images',
+		'with_front'          => true,
+		'pages'               => true,
+		'feeds'               => true,
+	);
+	$args = array(
+		'label'               => __( 'Images', 'text_domain' ),
+		'description'         => __( 'User submitted images', 'text_domain' ),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'page-attributes', ),
+		'hierarchical'        => true,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 5,
+		'menu_icon'           => 'dashicons-format-image',
+		'can_export'          => true,
+		'has_archive'         => true,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'rewrite'             => $rewrite,
+		'capability_type'     => 'post',
+	);
+	register_post_type( 'image', $args );
 
 }
 
