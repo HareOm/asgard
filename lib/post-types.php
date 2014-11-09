f7bcc04ce36e1f8815125a68505d39f5a14de591
Greg <greg@w3visi.com>
2014-11-09 18:46:06 -0500

    Added video custom post type

--- lib/post-types.php
+++ lib/post-types.php
@@ -142,6 +142,52 @@ function val_post_type() {
 	);
 	register_post_type( 'image', $args );
 
+	/*********************
+	VIDEO CPT
+	*********************/
+	$labels = array(
+		'name'                => _x( 'Videos', 'Post Type General Name', 'text_domain' ),
+		'singular_name'       => _x( 'Video', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'           => __( 'Videos', 'text_domain' ),
+		'parent_item_colon'   => __( 'Parent Video:', 'text_domain' ),
+		'all_items'           => __( 'Videos', 'text_domain' ),
+		'view_item'           => __( 'View Video', 'text_domain' ),
+		'add_new_item'        => __( 'Add New Video', 'text_domain' ),
+		'add_new'             => __( 'Add New', 'text_domain' ),
+		'edit_item'           => __( 'Edit Video', 'text_domain' ),
+		'update_item'         => __( 'Update Video', 'text_domain' ),
+		'search_items'        => __( 'Search Videos', 'text_domain' ),
+		'not_found'           => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
+	);
+	$rewrite = array(
+		'slug'                => 'videos',
+		'with_front'          => true,
+		'pages'               => true,
+		'feeds'               => true,
+	);
+	$args = array(
+		'label'               => __( 'Videos', 'text_domain' ),
+		'description'         => __( 'User submitted videos', 'text_domain' ),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'page-attributes', ),
+		'hierarchical'        => true,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 5,
+		'menu_icon'           => 'dashicons-format-video',
+		'can_export'          => true,
+		'has_archive'         => true,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'rewrite'             => $rewrite,
+		'capability_type'     => 'post',
+	);
+	register_post_type( 'video', $args );
+
 }
 
 // Hook into the 'init' action
