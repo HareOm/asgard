92a0cc0fb5d6d8962f8638ef3219ba7b73ab98ad
Greg <greg@w3visi.com>
2014-11-09 22:58:32 -0500

    Added post type filter to media page

--- index.php
+++ index.php
@@ -7,6 +7,45 @@
   <?php get_search_form(); ?>
 <?php endif; ?>
 
+
+<?php
+$post_type = get_post_type();
+$post_types = get_post_types(array(
+  "publicly_queryable" => true,
+));
+$exclude_post_types = array("attachment", "manifesto", "press_release");
+?>
+
+<ul class="nav nav-pills" role="tablist">
+<?php foreach($post_types as $pt): ?>
+  <?php if( !in_array($pt, $exclude_post_types) ): ?>
+  <?php
+  if( $pt == "post" ) {
+    $name = "Article";
+  } else {
+    $name = ucfirst($pt);
+  }
+
+  ?>
+  <li<?php if($post_type == $pt) echo ' class="active"' ?>><a href="#"><?php echo $name ?></a>
+  <?php endif ?>
+<?php endforeach; ?>
+</ul>
+
+
+<hr>
+<?php
+$term = get_query_var( "term" );
+$filters = get_terms_by_post_type(array("category"), array(get_post_type()));
+?>
+
+<ul class="nav nav-pills" role="tablist">
+<?php foreach($filters as $filter): ?>
+  <li<?php if($term == $filter->slug) echo ' class="active"' ?>><a href="<?php echo get_term_link($filter) ?>"><?php echo $filter->name ?></a>
+<?php endforeach; ?>
+</ul>
+
+
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/content', get_post_format()); ?>
 <?php endwhile; ?>
