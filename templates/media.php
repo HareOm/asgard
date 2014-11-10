b810bd15ffdcb7406caa264667ba30ce25d05efa
Greg <greg@w3visi.com>
2014-11-10 08:39:58 -0500

    Filtering on media template

---
+++ templates/media.php
@@ -0,0 +1,55 @@
+<?php
+/*
+* Template Name: Media
+*/
+?>
+<?php get_template_part('templates/page', 'header'); ?>
+
+<?php
+
+//Set the post type in the page template's custom field
+if( get_post_meta($post->ID, 'post_type', TRUE) ) {
+  $post_type = get_post_meta($post->ID, 'post_type', TRUE);
+} else {
+  $post_type = 'post';
+}
+
+$query = array(
+  "post_type" => $post_type,
+  "orderby" => "date",
+  "order" => "DESC"
+);
+$media_query = new WP_Query($query);
+
+?>
+
+<?php if (!$media_query->have_posts()) : ?>
+  <div class="alert alert-warning">
+    <?php _e('Sorry, no results were found.', 'roots'); ?>
+  </div>
+  <?php get_search_form(); ?>
+<?php endif; ?>
+
+<?php
+$term = get_query_var( "term" );
+$filters = get_terms_by_post_type(array("category"), array($post_type));
+?>
+
+<ul class="nav nav-pills" role="tablist">
+<?php foreach($filters as $filter): ?>
+  <li<?php if($term == $filter->slug) echo ' class="active"' ?>><a href="<?php echo get_term_link($filter) ?>"><?php echo $filter->name ?></a>
+<?php endforeach; ?>
+</ul>
+
+<?php while ($media_query->have_posts()) : $media_query->the_post(); ?>
+  <?php get_template_part('templates/content', get_post_format()); ?>
+<?php endwhile; ?>
+
+<?php if ($wp_query->max_num_pages > 1) : ?>
+  <nav class="post-nav">
+    <ul class="pager">
+      <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'roots')); ?></li>
+      <li class="next"><?php previous_posts_link(__('Newer posts &rarr;', 'roots')); ?></li>
+    </ul>
+  </nav>
+<?php endif; ?>
