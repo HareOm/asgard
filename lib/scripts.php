c17d523cf415c5680fe025581e57048b4aa3e363
Greg <greg@w3visi.com>
2014-12-28 17:58:37 -0500

    Tweaked menu mobile

--- lib/scripts.php
+++ lib/scripts.php
@@ -12,7 +12,7 @@
  */
 function roots_scripts() {
   global $google_maps_api_key;
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '87630dc224a20d81523e5f5be484c14d');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd3fab1a1c49c2c3cd2dcc16dda4b12d2');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
