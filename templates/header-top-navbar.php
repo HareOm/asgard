f043cb8e1a3569da9f6b75c5857fec3802b67d53
Greg <greg@w3visi.com>
2015-01-04 16:40:11 -0500

    Added tagline to header

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -18,6 +18,9 @@
           <span class="icon-bar"></span>
         </button>
         <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
+        <div class="navbar-text">
+          <h2 class="site-tagline"><a href="<?php echo home_url('index.php?page_id=3700') ?>"><?php bloginfo('description') ?></a></h2>
+        </div>
       </div>
 
       <nav class="collapse navbar-collapse navbar-right" role="navigation">
