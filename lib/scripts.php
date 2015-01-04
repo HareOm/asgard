c26bbfe23f5bb18af923e76304b947cdaccd1194
Greg <greg@w3visi.com>
2015-01-04 18:45:10 -0500

    Fixed vote display counter

--- lib/scripts.php
+++ lib/scripts.php
@@ -12,7 +12,7 @@
  */
 function roots_scripts() {
   global $google_maps_api_key;
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '67adb955c2265f804cd904687f299e5f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '447576148fb4d32948f3d9f1943aaa78');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
