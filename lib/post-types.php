c13dca0cbd3d9961e4c23808537e59f2698dc5c9
Greg <greg@w3visi.com>
2015-02-20 08:54:50 -0500

    Added post type for greenseed projects

--- lib/post-types.php
+++ lib/post-types.php
@@ -237,6 +237,54 @@ function val_post_type() {
 	);
 	register_post_type( 'link', $args );
 
+	/*********************
+	Greenseed Projects CPT
+	*********************/
+	$labels = array(
+		'name'                => _x( 'Projects', 'Post Type General Name', 'text_domain' ),
+		'singular_name'       => _x( 'Project', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'           => __( 'Projects', 'text_domain' ),
+		'parent_item_colon'   => __( 'Parent Project:', 'text_domain' ),
+		'all_items'           => __( 'Projects', 'text_domain' ),
+		'view_item'           => __( 'View Project', 'text_domain' ),
+		'add_new_item'        => __( 'Add New Project', 'text_domain' ),
+		'add_new'             => __( 'Add New', 'text_domain' ),
+		'edit_item'           => __( 'Edit Project', 'text_domain' ),
+		'update_item'         => __( 'Update Project', 'text_domain' ),
+		'search_items'        => __( 'Search Project', 'text_domain' ),
+		'not_found'           => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
+	);
+	$rewrite = array(
+		'slug'                => 'projects',
+		'with_front'          => true,
+		'pages'               => true,
+		'feeds'               => true,
+	);
+	$args = array(
+		'label'               => __( 'Projects', 'text_domain' ),
+		'description'         => __( 'User submitted images', 'text_domain' ),
+		'taxonomies'         	=> array('category', 'post_tag'),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'page-attributes', 'thumbnail', 'custom-fields'),
+		'hierarchical'        => true,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 5,
+		'menu_icon'           => 'dashicons-megaphone',
+		'can_export'          => true,
+		'has_archive'         => false,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'rewrite'             => $rewrite,
+		'capability_type'     => 'post',
+	);
+	register_post_type( 'ignition_product', $args );
+
+
 }
 
 // Hook into the 'init' action
